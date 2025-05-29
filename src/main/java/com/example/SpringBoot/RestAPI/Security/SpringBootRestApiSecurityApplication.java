ddd18d630e631cb6224f810d47dadbd5ee5f829e
pooja <morepooja235@gmail.com>
2025-05-29 18:53:15 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringBoot/RestAPI/Security/SpringBootRestApiSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot.RestAPI.Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot/RestAPI/Security/SpringBootRestApiSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot.RestAPI.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
